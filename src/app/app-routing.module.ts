aa5d63099c99f38505ce0de19542a31f915cff11
Marianne <laballery.marianne@orange.fr>
2021-07-30 17:01:16 +0200

    parametrage page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   {path: "", component: HomePageComponent},
   {path: "cars", component: CarsListComponent},
-  {path: "drivers", component: DriversListComponent}
+  {path: "drivers", component: DriversListComponent},
+  {path: "not-found", component: FourOhFourComponent},
+  {path: "**", redirectTo:"not-found"}
 ];
 
 @NgModule({
