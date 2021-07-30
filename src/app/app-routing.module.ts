8183cde20c36906050f7a224cfc75c3904848f85
Marianne <laballery.marianne@orange.fr>
2021-07-30 15:16:35 +0200

    parametrage header, creation component cars, drivers, home-page et 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomePageComponent},
+  {path: "cars", component: CarsListComponent},
+  {path: "drivers", component: DriversListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
